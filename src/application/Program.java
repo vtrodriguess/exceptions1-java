74638e5abdcb339978eccec6bc11edb2fff31dec
Antonio Vitor <mod.vtr@gmail.com>
2024-07-29 11:33:06 -0300

    very bad solution

---
+++ src/application/Program.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		// TODO Auto-generated method stub
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("Numero do quarto: ");
+		int number = sc.nextInt();
+		System.out.print("Data do check-in (dd/MM/yyyy): ");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.print("Data do check-out (dd/MM/yyyy): ");
+		Date checkOut = sdf.parse(sc.next());
+		
+		if(!checkOut.after(checkIn)) {
+			System.out.println("Error in reservation");
+		}
+		else {
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
+			System.out.println(reservation);
+			System.out.println();
+			System.out.println("Mude a reserva");
+			System.out.print("Data do check-in (dd/MM/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+			System.out.print("Data do check-out (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+			
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println(reservation);
+		}
+		
+		sc.close();
+		
+	}
+
+}
